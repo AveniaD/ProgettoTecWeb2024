0ecfe28b235c9f2b4eab8603e18f5746051e6db3
Davide Avenia <d.avenia.97@gmail.com>
2024-03-25 12:52:48 +0100

    First commit - start project

---
+++ innervision/innervision/src/main/java/com/uniparthenope/innervision/InnervisionApplication.java
@@ -0,0 +1,13 @@
+package com.uniparthenope.innervision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnervisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnervisionApplication.class, args);
+	}
+
+}
---
+++ innervision/innervision/src/test/java/com/uniparthenope/innervision/InnervisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniparthenope.innervision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnervisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
